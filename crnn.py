f9bd6fafb7d9ae9f592557ba3db65c5f386e7732
cooper <cooper.yi@outlook.com>
2018-03-07 17:05:40 +0800

    train part

--- crnn.py
+++ crnn.py
@@ -2,21 +2,26 @@ import os
 import tensorflow as tf 
 from tensorflow.contrib import layers
 from tensorflow.contrib import rnn
+from data_manager import DataManager
 import config
 
 class CRNN:
-    def __init__(self, batch_size, data, label):
+    def __init__(self, batch_size, epoches, data_path):
         self.batch_size = batch_size
-        self.data = data
-        self.label = label
+        self.epoches = epoches
+        self.data = DataManager(data_path, batch_size)
 
-        self.input = tf.placeholder(tf.float32, [self.batch_size, 32, -1, 1])
-        self.target = tf.sparse_placeholder(tf.int32, [None])
-        self.seqLength = tf.placeholder(tf.int32, [None])
+        self.sess = tf.Session()
 
-        self.build_model(self.input)
-    
-    def build_model(self, input):
+        with self.sess.as_default():   
+            self.inputs, self.targets, self.seq_len, self.logits, self.decoded, self.optimizer, self.accracy, self.losses,self.initlizer =  self.build_model()
+            self.initlizer.run()
+
+    def build_model(self):
+        input = tf.placeholder(tf.float32, [self.batch_size, 32, -1, 1])
+        target = tf.sparse_placeholder(tf.int32, [None])
+        seqLength = tf.placeholder(tf.int32, [None])
+
         # CNN layers
         conv_layer1 = layers.conv2d(inputs=input, 64, kernel_size=(3,3), strides=1, padding="SAME")
         pool_layer1 = layers.max_pooling2d(inputs=conv_layer1, pool_size=(2, 2), strides=2)
@@ -57,21 +62,41 @@ class CRNN:
         predict_out = tf.transpose(logits, (1, 0, 2))  #  Permutes the dimensions according to perm(axis location arrange)
 
         # loss value to be optimized
-        self.loss = tf.reduce_mean(tf.nn.ctc_loss(self.target, self.predict_out, self.seqLength))
+        loss = tf.reduce_mean(tf.nn.ctc_loss(target, self.predict_out, seqLength))
+
+        # optimizer
+        optimizer = tf.train.MomentumOptimizer(0.01, 0.9).minimize(loss)
 
         # the predict string
-        self.predict_string, prob = tf.nn.ctc_beam_search_decoder(predict_out, self.seqLength)
+        predict_string, prob = tf.nn.ctc_beam_search_decoder(predict_out, seqLength)
 
         # accuracy for string predict
-        self.acc = tf.reduce_mean(tf.edit_distance(tf.cast(self.predict_string[0], tf.int32), self.targets))
+        acc = tf.reduce_mean(tf.edit_distance(tf.cast(self.predict_string[0], tf.int32), target))
+
+        # init 
+        init = tf.global_variables_initializer()
+
+        return input, target, seqLength, predict_out, predict_string, optimizer, acc, loss, init
 
     # train and save model
     def train(self):
-        pass
+        with self.sess.as_default():
+            for i in range(self.epoches):
+                iteration_loss = 0
+                for batch_x, batch_y, batch_length in self.data.get_next_train_batch():  #TODO
+                    data_targets = np.asarray([label_to_array(label, config.CHAR_NUMBERS) for label in batch_y])  #TODO
+                    data_targets = sparse_tuple_from(data_targets)  # TODO
+                    temp3, loss_val, predict_str = self.sess.run(
+                        [self.optimizer, self.losses, self.decoded],
+                        feed_dict = {self.inputs:batch_x, self.targets:batch_y, self.seq_len:batch_length}
+                    )
+                    iteration_loss += loss_val
+                print "Iteration {} : loss: {}".format(i, iteration_loss)
+        return None
     
     # test the trained model
     def test(self):
-        pass        
+        pass
 
 
 
