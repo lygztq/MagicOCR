fc8234ef5383aa2c8479089f37342559c6cf8133
cooper <cooper.yi@outlook.com>
2018-03-08 16:12:07 +0800

    functions and interface

--- crnn.py
+++ crnn.py
@@ -4,8 +4,9 @@ from tensorflow.contrib import layers
 from tensorflow.contrib import rnn
 from data_manager import DataManager
 import config
+from utils import label_to_array, levenshtein, sparse_tuple_from, ground_truth_to_word
 
-class CRNN:
+class CRNN(object):
     def __init__(self, batch_size, epoches, data_path):
         self.batch_size = batch_size
         self.epoches = epoches
@@ -19,8 +20,8 @@ class CRNN:
 
     def build_model(self):
         input = tf.placeholder(tf.float32, [self.batch_size, 32, -1, 1])
-        target = tf.sparse_placeholder(tf.int32, [None])
-        seqLength = tf.placeholder(tf.int32, [None])
+        target = tf.sparse_placeholder(tf.int32, name='targets')
+        seqLength = tf.placeholder(tf.int32, [None], name='seqLength')
 
         # CNN layers
         conv_layer1 = layers.conv2d(inputs=input, 64, kernel_size=(3,3), strides=1, padding="SAME")
@@ -58,7 +59,7 @@ class CRNN:
         logits = tf.matmul(logits, weight) + bias  # https://docs.scipy.org/doc/numpy/user/basics.broadcasting.html
         logits = tf.reshape(logits, [self.batch_size, -1, config.CHAR_NUMBERS])  # [batch_size, 1, CHAR_NUMBERS]
 
-        # outputs    [[[1], [1], ... batch_size number ..., [1]], ... CHAR_NUMBER ... ]   '1' stands predict char with dtype=tf.int32
+        # outputs    [[[1], [1], ... batch_size number ..., [1]], ... CHAR_NUMBER ... ]   '1' stands predict char index in CHAR_DICTIONARY
         predict_out = tf.transpose(logits, (1, 0, 2))  #  Permutes the dimensions according to perm(axis location arrange)
 
         # loss value to be optimized
@@ -83,12 +84,12 @@ class CRNN:
         with self.sess.as_default():
             for i in range(self.epoches):
                 iteration_loss = 0
-                for batch_x, batch_y, batch_length in self.data.get_next_train_batch():  #TODO
-                    data_targets = np.asarray([label_to_array(label, config.CHAR_NUMBERS) for label in batch_y])  #TODO
-                    data_targets = sparse_tuple_from(data_targets)  # TODO
+                for batch_x, batch_y, batch_length in self.data.get_next_train_batch():  #TODO1
+                    data_targets = np.asarray([label_to_array(label, config.CHAR_DICTIONARYS) for label in batch_y])
+                    data_targets = sparse_tuple_from(data_targets)
                     temp3, loss_val, predict_str = self.sess.run(
                         [self.optimizer, self.losses, self.decoded],
-                        feed_dict = {self.inputs:batch_x, self.targets:batch_y, self.seq_len:batch_length}
+                        feed_dict = {self.inputs:batch_x, self.targets:data_targets, self.seq_len:batch_length}
                     )
                     iteration_loss += loss_val
                 print "Iteration {} : loss: {}".format(i, iteration_loss)
@@ -99,15 +100,15 @@ class CRNN:
         with self.sess.as_default():
             example_count = 0
             total_error = 0
-            for batch_x, batch_y, batch_length in self.data.get_next_test_batch():  #TODO
-                data_targets = np.asarray([label_to_array(label, config.CHAR_NUMBERS) for label in batch_y])
+            for batch_x, batch_y, batch_length in self.data.get_next_test_batch():  #TODO2
+                data_targets = np.asarray([label_to_array(label, config.CHAR_DICTIONARYS) for label in batch_y])
                 data_targets = sparse_tuple_from(data_targets)
                 predict_str = self.sess.run(
                     [self.decoded],
                     feed_dict={self.inputs:batch_x, self.seq_len:batch_length}
                 )
                 example_count += len(batch_y)
-                total_error += np.sum(levenshtein(ground_truth_to_word(batch_y), ground_truth_to_word(decoded)))  # TODO
+                total_error += np.sum(levenshtein(ground_truth_to_word(batch_y), ground_truth_to_word(decoded)))
             print "Error on test set: {}".format(total_error/example_count)
         return None
 
